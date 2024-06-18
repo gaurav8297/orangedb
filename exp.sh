ff1464758f31e4ba8f9e06f3b5f1c40eaa7635fa
Gaurav Sehgal <gaurav.sehgal8297@gmail.com>
2024-06-18 00:00:15 -0400

    Adaptive alpha optimization

--- exp.sh
+++ exp.sh
@@ -1,10 +1,43 @@
+#!/bin/bash
 
+# Function to execute a command with retries
+function execute_with_retry {
+    local command="$1"
+    local max_retries=3
+    local retry_count=0
 
-./orangedb_main -basePath /home/g3sehgal/vector_index_exp/gist -nCentroids 10 -nIter 20 -minCentroidSize 10000 -maxCentroidSize 20000 -M 64 -K 100 -efConstruction 200 -efSearch 200 -nThreads 32 --minCentroids 4 -centroidThreshold 1
+    while [ $retry_count -lt $max_retries ]; do
+        $command  # Execute the command
 
-./orangedb_main -basePath /home/g3sehgal/vector_index_exp/gist -M 64 -K 100 -efConstruction 200 -efSearch 200 -nThreads 16 -numVectors 1000000 -alpha 1.0 -nCentroids 10 -nIter 60 -minCentroidSize 15000 -maxCentroidSize 20000 -maxSearchCentroids 10 -searchThreshold 0.5 -deletePercent 0.05
+        if [ $? -eq 0 ]; then
+            echo "Command executed successfully."
+            break
+        else
+            echo "Command failed. Retrying..."
+            ((retry_count++))
+        fi
+    done
 
-./orangedb_main -basePath /home/g3sehgal/vector_index/vector_index/data/siftsmall -M 64 -K 100 -efConstruction 200 -efSearch 200 -nThreads 16 -numVectors 10000 -alpha 1.0
+    if [ $retry_count -eq $max_retries ]; then
+        echo "Command failed after $max_retries retries. Exiting."
+        exit 1
+    fi
+}
 
-./orangedb_main -basePath /Users/gauravsehgal/work/vector_index/data/siftsmall -M 64 -K 100 -efConstruction 200 -efSearch 200 -nThreads 16 -numVectors 10000 -alpha 1.0 -nCentroids 10 -nIter 20 -minCentroidSize 500 -maxCentroidSize 500 -maxSearchCentroids 3 -searchThreshold 0.5
+# Retry the below commands if they fail
 
+execute_with_retry "./orangedb_main -basePath /home/g3sehgal/vector_index_exp/gist -M 64 -K 100 -efConstruction 200 -efSearch 200 -nThreads 32 -numVectors 1000000 -minAlpha 1.2 -maxAlpha 1.2 -alphaDecay 0.0 >> baseline_1.2.txt"
+
+execute_with_retry "./orangedb_main -basePath /home/g3sehgal/vector_index_exp/gist -M 64 -K 100 -efConstruction 200 -efSearch 200 -nThreads 32 -numVectors 1000000 -minAlpha 1.15 -maxAlpha 1.15 -alphaDecay 0.0 >> baseline_1.15.txt"
+
+execute_with_retry "./orangedb_main -basePath /home/g3sehgal/vector_index_exp/gist -M 64 -K 100 -efConstruction 200 -efSearch 200 -nThreads 32 -numVectors 1000000 -minAlpha 0.95 -maxAlpha 1.2 -alphaDecay 0.02 >> output1.txt"
+
+execute_with_retry "./orangedb_main -basePath /home/g3sehgal/vector_index_exp/gist -M 64 -K 100 -efConstruction 200 -efSearch 200 -nThreads 32 -numVectors 1000000 -minAlpha 0.95 -maxAlpha 1.2 -alphaDecay 0.04 >> output2.txt"
+
+execute_with_retry "./orangedb_main -basePath /home/g3sehgal/vector_index_exp/gist -M 64 -K 100 -efConstruction 200 -efSearch 200 -nThreads 32 -numVectors 1000000 -minAlpha 0.95 -maxAlpha 1.2 -alphaDecay 0.06 >> output3.txt"
+
+execute_with_retry "./orangedb_main -basePath /home/g3sehgal/vector_index_exp/gist -M 64 -K 100 -efConstruction 200 -efSearch 200 -nThreads 32 -numVectors 1000000 -minAlpha 0.95 -maxAlpha 1.2 -alphaDecay 0.08 >> output4.txt"
+
+execute_with_retry "./orangedb_main -basePath /home/g3sehgal/vector_index_exp/gist -M 64 -K 100 -efConstruction 200 -efSearch 200 -nThreads 32 -numVectors 1000000 -minAlpha 0.95 -maxAlpha 1.2 -alphaDecay 0.1 >> output5.txt"
+
+execute_with_retry "./orangedb_main -basePath /home/g3sehgal/vector_index_exp/gist -M 64 -K 100 -efConstruction 200 -efSearch 200 -nThreads 32 -numVectors 1000000 -minAlpha 0.95 -maxAlpha 1.2 -alphaDecay 0.2 >> output6.txt"
