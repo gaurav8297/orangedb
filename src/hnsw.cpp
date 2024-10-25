22c4fb23fdd40f321cade77b7b7723e88e44be86
Gaurav Sehgal <gaurav.sehgal8297@gmail.com>
2024-10-25 06:13:10 -0400

    some stuff

--- src/hnsw.cpp
+++ src/hnsw.cpp
@@ -1640,7 +1640,7 @@ namespace orangedb {
                 continue;
             }
             depth = std::max(depth, candidate.second);
-            if (depth >= 3 || nbrs.size() >= maxK) {
+            if (depth >= 3) {
                 return (depth - 1);
             }
             visitedSet.insert(candidate.first);
