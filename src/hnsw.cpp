8294eb6f123e132295ff6d08df80934d0196998f
Gaurav Sehgal <gaurav.sehgal8297@gmail.com>
2024-10-26 20:08:58 -0400

    some stuff

--- src/hnsw.cpp
+++ src/hnsw.cpp
@@ -1804,7 +1804,7 @@ namespace orangedb {
             searchNearestOnLevel(&dc, level, nearestID, nearestDist, stats);
             nearestID = storage->next_level_ids[level][nearestID];
         }
-        searchNeighborsOnLastLevelWithFilterA(
+        searchNeighborsOnLastLevelWithFilterB(
                 &dc,
                 results,
                 nearestID,
