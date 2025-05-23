42ab8bc7e59abb24702ba78bde60d60ba6bdbd68
gaurav8297 <gaurav.sehgal8297@gmail.com>
2025-05-23 16:54:52 +0530

    WIP: Reclustering Approach

--- src/main.cpp
+++ src/main.cpp
@@ -1434,7 +1434,7 @@ void benchmark_acorn(InputParser &input) {
                 printf("Recall: %f\n", recall);
                 auto recallPerQuery = recall / queryNumVectors;
                 return recallPerQuery / k;
-            }, minRecall, maxRecall, 100, 10000, 50);
+            }, minRecall, maxRecall, 100, 1500, 50);
             acorn_index->acorn.efSearch = ef;
             efSearch = ef;
         } else {
