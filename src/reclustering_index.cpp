9f863230a0fa35ecb62d2e612c4c4a37ea44ec4e
gaurav8297 <gaurav.sehgal8297@gmail.com>
2025-06-12 14:05:22 +0530

    WIP: Reclustering Approach

--- src/reclustering_index.cpp
+++ src/reclustering_index.cpp
@@ -1118,7 +1118,7 @@ namespace orangedb {
         findKClosestMegaCentroids(query, nMegaProbes, megaAssign);
 
         auto numMicroCentroids = miniCentroids.size() / dim;
-        auto dc = getDistanceComputer(miniCentroids.data(), numMicroCentroids);
+        auto dc = getQuantizedDistanceComputer(quantizedMiniCentroids.data(), numMicroCentroids);
         dc->setQuery(query);
 
         // Now find the closest micro centroids
