b3bcabe8b415921762b753449353ea27eb5c8144
gaurav8297 <gaurav.sehgal8297@gmail.com>
2025-06-20 18:13:13 +0530

    WIP: Reclustering Approach

--- src/reclustering_index.cpp
+++ src/reclustering_index.cpp
@@ -1534,21 +1534,21 @@ namespace orangedb {
         printf("ReclusteringIndex::printStats\n");
         // Print the number of mega clusters
         printf("Number of mega clusters: %zu\n", megaCentroids.size() / dim);
-        printf("Number of mini clusters: %zu\n", miniCentroids.size() / dim);
-        // print min, max, avg size of the clusters
-        auto minSize = std::numeric_limits<size_t>::max();
-        size_t maxSize = 0;
-        size_t avgSize = 0;
-        for (const auto &cluster: miniClusters) {
-            auto size = cluster.size() / dim;
-            minSize = std::min(minSize, size);
-            maxSize = std::max(maxSize, size);
-            avgSize += size;
-        }
-        printf("Min size of clusters: %zu\n", minSize);
-        printf("Max size of clusters: %zu\n", maxSize);
-        printf("Avg size of clusters: %zu\n", avgSize / miniClusters.size());
-        printf("Total number of vectors: %zu/%zu\n", avgSize, size);
+        // printf("Number of mini clusters: %zu\n", miniCentroids.size() / dim);
+        // // print min, max, avg size of the clusters
+        // auto minSize = std::numeric_limits<size_t>::max();
+        // size_t maxSize = 0;
+        // size_t avgSize = 0;
+        // for (const auto &cluster: miniClusters) {
+        //     auto size = cluster.size() / dim;
+        //     minSize = std::min(minSize, size);
+        //     maxSize = std::max(maxSize, size);
+        //     avgSize += size;
+        // }
+        // printf("Min size of clusters: %zu\n", minSize);
+        // printf("Max size of clusters: %zu\n", maxSize);
+        // printf("Avg size of clusters: %zu\n", avgSize / miniClusters.size());
+        // printf("Total number of vectors: %zu/%zu\n", avgSize, size);
 
         printf("Number of quantized mini clusters: %zu\n", quantizedMiniCentroids.size() / quantizer->codeSize);
         // print min, max, avg size of the quantized clusters
