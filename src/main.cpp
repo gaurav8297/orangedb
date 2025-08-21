69ed3e30a31d1d2cc9fb834c9de780ac492ebc86
gaurav8297 <gaurav.sehgal8297@gmail.com>
2025-08-21 18:40:55 +0530

    WIP: Reclustering Approach

--- src/main.cpp
+++ src/main.cpp
@@ -2541,6 +2541,7 @@ void benchmark_fast_reclustering(InputParser &input) {
     //                                             nMegaProbes, nMiniProbes);
     // printf("Recall: %f, Recall without bad clusters: %f, Recall with bad clusters: %f\n", recall, recallWithoutBadClusters, recallWithBadCluster);
     index.printStats();
+    auto recall = 0.0;
 
     // index.storeScoreForMegaClusters();
     // index.flush_to_disk(storagePath);
