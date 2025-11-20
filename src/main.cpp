9741972ff836a053df10eec2c6900c18ee8df5c8
gaurav8297 <gaurav.sehgal8297@gmail.com>
2025-11-20 16:19:20 +0530

    WIP: Reclustering Approach

--- src/main.cpp
+++ src/main.cpp
@@ -2455,7 +2455,7 @@ double get_recall(ReclusteringIndex &index, float *queryVecs, size_t queryDimens
         }
     }
     printf("Avg Distance Computation: %llu\n", stats.numDistanceCompForSearch / queryNumVectors);
-    printf("Max Recall: %f, Min Recall: %f, Num Recall below 70%%: %f\n", max_recall, min_recall, num_recall_below_70);
+    printf("Max Recall: %f, Min Recall: %f, Num Recall below 75%%: %f\n", max_recall, min_recall, num_recall_below_75);
     return recall / queryNumVectors;
 }
 
